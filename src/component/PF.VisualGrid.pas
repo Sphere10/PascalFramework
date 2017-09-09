70ef966a2718fdbc3278ce0475a93bdffbe0ad98
maciej-izak <hnb.code@gmail.com>
2017-09-09 23:51:09 +0200

    * Paging for VisualGrid :) * Small refactoring and bug fixes

--- src/component/PF.VisualGrid.pas
+++ src/component/PF.VisualGrid.pas
@@ -25,6 +25,8 @@ type
     FilterNumeric2: Int64;
   end;
 
+  TArray<T> = array of T;
+
   TFilterCriteria = TArray<TColumnFilter>;
 
   TDataTable = record
@@ -35,13 +37,18 @@ type
   end;
 
   IDataSource = interface
-    function FetchPage(APageNumber, APageSize : Integer; out APageCount : Integer; AFilter: TFilterCriteria): TDataTable;
+    function FetchPage(APageIndex, APageSize : Integer; out APageCount : Integer; AFilter: TFilterCriteria): TDataTable;
   end;
 
   TDrawVisualCellEvent = procedure(Sender: TObject; ACol, ARow: Longint;
     Rect: TRect; State: TGridDrawState; const RowData: Variant; var Handled: boolean) of object;
 
   TCustomVisualGrid = class(TWinControl)
+  protected const
+    PAGE_NAVIGATION_FIRST    = 1;
+    PAGE_NAVIGATION_PREVIOUS = 2;
+    PAGE_NAVIGATION_NEXT     = 3;
+    PAGE_NAVIGATION_LAST     = 4;
   protected { component interface part }
     FSearchLabel: TLabel;
     FSearchEdit: TEdit;
@@ -56,21 +63,32 @@ type
     FButtonNext: TButton;
     FButtonPrevious: TButton;
 
-    FCurrentPageEdit: TEdit;
-    FAllPagesLabel: TLabel;
+    FPageIndexEdit: TEdit;
+    FPageCountLabel: TLabel;
 
     FPageSizeEdit: TEdit;
     FPageSizeLabel: TLabel;
     FAllRecordsCountLabel: TLabel;
 
     FDrawGrid: TDrawGrid;
+  protected { events for UI }
+    procedure StandardDrawCell(Sender: TObject; ACol, ARow: Longint;
+      Rect: TRect; State: TGridDrawState);
+    procedure PageIndexEditChange(Sender: TObject);
+    procedure PageNavigationClick(Sender: TObject);
   private
     procedure ClickTest(Sender: TObject);
+    procedure SetPageIndex(const Value: Integer);
+    procedure SetPageSize(const Value: Integer);
   protected { TComponent }
     procedure Loaded; override;
   protected
+    FPageIndexUpdate: boolean;
     FDataTable: TDataTable;
     FDataSource: IDataSource;
+    FPageSize: Integer;
+    FPageIndex: Integer;
+    FPageCount: Integer;
 
     FColumns: TStrings;
     FOnDrawVisualCell: TDrawVisualCellEvent;
@@ -79,23 +97,28 @@ type
     procedure RefreshGrid;
     procedure ReloadColumns;
     procedure SetDataSource(ADataSource: IDataSource);
-    procedure StandardDrawCell(Sender: TObject; ACol, ARow: Longint;
-    Rect: TRect; State: TGridDrawState);
     procedure DoDrawCell(Sender: TObject; ACol, ARow: Longint;
       Rect: TRect; State: TGridDrawState; const RowData: Variant);
-
-    property OnDrawVisualCell: TDrawVisualCellEvent read FOnDrawVisualCell write FOnDrawVisualCell;
+    procedure RefreshPageIndexInterface;
+    procedure RefreshPageIndexData(ARefreshColumns: boolean);
+    procedure SetPageIndexEditText(const AStr: utf8string);
   public
     constructor Create(Owner: TComponent); override;
     destructor Destroy; override;
     property Columns: TStrings read FColumns write SetColumns;
     property DataSource: IDataSource read FDataSource write SetDataSource;
+    property PageSize: Integer read FPageSize write SetPageSize default 100;
+    property PageIndex: Integer read FPageIndex write SetPageIndex default -1;
+
+    property OnDrawVisualCell: TDrawVisualCellEvent read FOnDrawVisualCell write FOnDrawVisualCell;
   end;
 
   TVisualGrid = class(TCustomVisualGrid)
   published
     property Align;
     property Columns;
+    property PageSize;
+
     property OnDrawVisualCell;
   end;
 
@@ -119,7 +142,9 @@ end;
 constructor TCustomVisualGrid.Create(Owner: TComponent);
 begin
   inherited;
-  FColumns := TStringList.Create;
+
+  { component layout }
+
   ControlStyle := ControlStyle - [csAcceptsControls];
 
   FTopPanel := TPanel.Create(Self);
@@ -182,18 +207,19 @@ begin
       Height := 40;
       Align := alRight;
       BevelOuter := bvNone;
-      FCurrentPageEdit := TEdit.Create(Self);
-      FCurrentPageEdit.Parent := FBottomRightPanel;
-      with FCurrentPageEdit do
+      FPageIndexEdit := TEdit.Create(Self);
+      FPageIndexEdit.Parent := FBottomRightPanel;
+      with FPageIndexEdit do
       begin
         Left := 61;
         Top := 10;
         Width := 56;
         Height := 21;
+        OnChange := PageIndexEditChange;
       end;
-      FAllPagesLabel := TLabel.Create(Self);
-      FAllPagesLabel.Parent := FBottomRightPanel;
-      with FAllPagesLabel do
+      FPageCountLabel := TLabel.Create(Self);
+      FPageCountLabel.Parent := FBottomRightPanel;
+      with FPageCountLabel do
       begin
         Left := 118;
         Top := 13;
@@ -210,6 +236,8 @@ begin
         Width := 25;
         Height := 25;
         Caption := #9198;
+        OnClick := PageNavigationClick;
+        Tag := PAGE_NAVIGATION_FIRST;
       end;
       FButtonPrevious := TButton.Create(Self);
       FButtonPrevious.Parent := FBottomRightPanel;
@@ -220,6 +248,8 @@ begin
         Width := 25;
         Height := 25;
         Caption := #9204;
+        OnClick := PageNavigationClick;
+        Tag := PAGE_NAVIGATION_PREVIOUS;
       end;
       FButtonNext := TButton.Create(Self);
       FButtonNext.Parent := FBottomRightPanel;
@@ -230,6 +260,8 @@ begin
         Width := 25;
         Height := 25;
         Caption := #9205;
+        OnClick := PageNavigationClick;
+        Tag := PAGE_NAVIGATION_NEXT;
       end;
       FButtonLast := TButton.Create(Self);
       FButtonLast.Parent := FBottomRightPanel;
@@ -240,6 +272,8 @@ begin
         Width := 25;
         Height := 25;
         Caption := #9197;
+        OnClick := PageNavigationClick;
+        Tag := PAGE_NAVIGATION_LAST;
       end;
     end;
 
@@ -287,6 +321,12 @@ begin
   FDrawGrid.Align := alClient;
   FDrawGrid.OnDrawCell := StandardDrawCell;
 
+  { default values for properties }
+
+  FColumns := TStringList.Create;
+  FPageSize := 100;
+  PageIndex := -1;
+
   with TButton.Create(Self) do
   begin
     Left := 0;
@@ -305,12 +345,17 @@ end;
 
 procedure TCustomVisualGrid.DoDrawCell(Sender: TObject; ACol, ARow: Integer;
   Rect: TRect; State: TGridDrawState; const RowData: Variant);
+var
+  LText: utf8string;
 begin
   if ARow = 0 then
-    FDrawGrid.Canvas.TextRect(Rect, Rect.Left+2, Rect.Top+2, FColumns[ACol])
+  begin
+    if ACol < FColumns.Count then
+      LText := FColumns[ACol];
+    FDrawGrid.Canvas.TextRect(Rect, Rect.Left+2, Rect.Top+2, LText)
+  end
   else
     FDrawGrid.Canvas.TextRect(Rect, Rect.Left+2, Rect.Top+2, RowData);
-
 end;
 
 procedure TCustomVisualGrid.Loaded;
@@ -319,9 +364,77 @@ begin
   ReloadColumns;
 end;
 
+procedure TCustomVisualGrid.PageIndexEditChange(Sender: TObject);
+var
+  LPageIndex: Integer;
+begin
+  if FPageIndexUpdate then
+    Exit;
+  // value in edit has increased value by 1 (more readable for end user)
+  LPageIndex := Pred(StrToIntDef(FPageIndexEdit.Text, Succ(FPageIndex)));
+  if (LPageIndex < 0) then
+  begin
+    LPageIndex := 0;
+    SetPageIndexEditText('1');
+  end;
+  if (LPageIndex > FPageCount-1) then
+  begin
+    LPageIndex := FPageCount-1;
+    SetPageIndexEditText(IntToStr(FPageCount));
+  end;
+
+  PageIndex := LPageIndex;
+end;
+
+procedure TCustomVisualGrid.PageNavigationClick(Sender: TObject);
+begin
+  if FPageCount = 0 then
+    Exit;
+  case TButton(Sender).Tag of
+    PAGE_NAVIGATION_FIRST: PageIndex := 0;
+    PAGE_NAVIGATION_PREVIOUS: PageIndex := PageIndex - 1;
+    PAGE_NAVIGATION_NEXT: PageIndex := PageIndex + 1;
+    PAGE_NAVIGATION_LAST: PageIndex := FPageCount - 1;
+  end;
+end;
+
 procedure TCustomVisualGrid.RefreshGrid;
 begin
 
+end;
+
+procedure TCustomVisualGrid.RefreshPageIndexData(ARefreshColumns: boolean);
+var
+  LFilter: TFilterCriteria;
+  i: Integer;
+begin
+  if Assigned(FDataSource) then
+  begin
+    if FPageIndex >= FPageCount then
+      FPageIndex := FPageCount - 1;
+
+    FDataTable := FDataSource.FetchPage(FPageIndex, FPageSize, FPageCount, LFilter);
+
+    if (FPageIndex = -1) and (Length(FDataTable.Rows) > 0) then
+      FPageIndex := 0;
+
+    if ARefreshColumns then
+    begin
+      FColumns.Clear;
+      for i := 0 to High(FDataTable.Columns) do
+        FColumns.Add(FDataTable.Columns[i]);
+      ReloadColumns;
+    end;
+    FDrawGrid.RowCount := Length(FDataTable.Rows) + 1;
+  end;
+  RefreshPageIndexInterface;
+  FDrawGrid.Refresh;
+end;
+
+procedure TCustomVisualGrid.RefreshPageIndexInterface;
+begin
+  SetPageIndexEditText(IntToStr(Succ(FPageIndex)));
+  FPageCountLabel.Caption := Format('/%d',[FPageCount]);
 end;
 
 procedure TCustomVisualGrid.ReloadColumns;
@@ -338,23 +451,35 @@ begin
 end;
 
 procedure TCustomVisualGrid.SetDataSource(ADataSource: IDataSource);
-var
-  LPages: Integer;
-  LFilter: TFilterCriteria;
-  i: Integer;
 begin
   if FDataSource = ADataSource then
     Exit;
 
   FDataSource := ADataSource;
-  FDataTable := FDataSource.FetchPage(1, 100, LPages, LFilter);
 
-  FColumns.Clear;
-  for i := 0 to High(FDataTable.Columns) do
-    FColumns.Add(FDataTable.Columns[i]);
-  ReloadColumns;
+  RefreshPageIndexData(true);
+end;
 
-  FDrawGrid.RowCount := 100;
+procedure TCustomVisualGrid.SetPageIndex(const Value: Integer);
+begin
+  if FPageIndex = Value then
+    Exit;
+
+
+  FPageIndex := Value;
+  RefreshPageIndexData(false)
+end;
+
+procedure TCustomVisualGrid.SetPageIndexEditText(const AStr: utf8string);
+begin
+  FPageIndexUpdate := True;
+  FPageIndexEdit.Text := AStr;
+  FPageIndexUpdate := false;
+end;
+
+procedure TCustomVisualGrid.SetPageSize(const Value: Integer);
+begin
+  FPageSize := Value;
 end;
 
 procedure TCustomVisualGrid.StandardDrawCell(Sender: TObject; ACol,
@@ -365,7 +490,7 @@ var
 begin
   LHandled := False;
   if (ARow > 0) and Assigned(FDataSource) then
-    LCellData := FDataTable.Rows[ARow]._(ACol);
+    LCellData := FDataTable.Rows[ARow-1]._(ACol);
 
   if Assigned(FOnDrawVisualCell) then
     FOnDrawVisualCell(Self, ACol, ARow, Rect, State, LCellData, LHandled);
